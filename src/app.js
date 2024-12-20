d806ea866ff8fca9ceb7488e81418ba9a65569e8
Olukemi Fasuan <ayanfe1711@gmail.com>
2024-12-20 14:28:16 +0100

    added protected admin route to app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,13 @@ const cors = require('cors');
 const app = express();
 const userRoutes = require('../routes/v1/userRoutes');
 const apartmentRoutes = require('../routes/v1/apartmentRoutes');
+const adminRoutes = require('../routes/v1/adminRoutes');
 
 app.use(cors());
 app.use(express.json());
 app.use('/v1/users', userRoutes);
 app.use('/v1/apartments', apartmentRoutes);
+app.use('/v1/admin', adminRoutes);
+
 
 module.exports = app;
